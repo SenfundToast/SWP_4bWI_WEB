38e7b98397335a386d242d51e49c832272c7217a
mathias <mathias@beirer.com>
2023-10-06 13:53:17 +0200

    js2_ajax - finished

--- Javascript_Ajax/js2/script.js
+++ Javascript_Ajax/js2/script.js
@@ -1,4 +1,20 @@
 const loadData = () => {
-    console.log("hello");
+    fetch("https://api.openligadb.de/getbltable/bl1/2022").then((result) => {
+        result.json().then((data) => {
+            fillTable(data);
+        });
+    });
+};
+
+const fillTable = (data) => {
+
+    let html = ""
+
+    data.forEach(element => {
+        html += "<div>" + element.points + "by Team " + element.teamName + "</div>";
+    });
+    document.getElementById("output").innerHTML = html;
+
 }
+
 loadData();
