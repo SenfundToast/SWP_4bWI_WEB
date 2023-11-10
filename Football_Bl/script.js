7dfe5bdcaaabe7b582db0ea19181d17789102553
mathias <mathias@beirer.com>
2023-11-10 17:03:53 +0100

    Tabel_1 added

--- Football_Bl/script.js
+++ Football_Bl/script.js
@@ -1,6 +1,7 @@
 function loadTable() {
     fetch("https://api.openligadb.de/getbltable/bl1/2022").then((result) => {
         result.json().then((result) => {
+            console.log(result)
             insertTable(result);
         });
     });
@@ -10,22 +11,45 @@ function loadTable() {
 function insertTable(data) {
     const element = document.getElementById("table_1");
 
-    let output = '';
-    array.forEach(team => {
-        html += `<div class="grid grid-cols-9">
-        <div class="col-span-1"> ${team.draw} </div>
-        <div class="col-span-1">logo</div>
+    let html = "";
+
+    data.forEach((team, index) => {
+        html += `
+        <div class="bg-firstplays w-[100vw]  grid grid-cols-3 items-center text-center text-white mt-5 border-2 border-neutral-900">
+            
+            <div class="grid grid-cols-9">
+                <div class="col-span-1 ">${index +1}</div>
+                <div class="col-span-1"><img width="35px" src="${team.teamIconUrl}"></div>
+                <div class="col-span-5">${team.teamName}</div>
+                <div class="col-span-2">${team.matches}</div>
+            </div>
+
+            <div class="grid grid-cols-3">
+                <div class="col-span-1">${team.won}</div>
+                <div class="col-span-1">${team.draw}</div>
+                <div class="col-span-1">${team.lost}</div>
+            </div>
+
+            <div class="grid grid-cols-4">
+                <div class="col-span-2">${team.goals}</div>
+                <div class="col-span-2">${team.points}</div>
+            </div>
+
         </div>`;
     });
 
-    element.innerHTML = output
-        //strg + d um gewisse sachen zu markieren
-        //strg + k + c
+    element.innerHTML = html;
+
+    //strg + d um gewisse sachen zu markieren
+    //strg + k + c
 
     //     
-    //     <div class="col-span-5 hover:text-clifford">Team name</div>
+    //  <div class="grid grid-cols-9">
+    //     <div class="col-span-1 ">num</div>
+    //     <div class="col-span-1">logo</div>
+    //     <div class="col-span-5">Team name</div>
     //     <div class="col-span-2">Matches</div>
-    // </div>
+    //  </div>
 
     // <div class="grid grid-cols-3">
     //     <div class="col-span-1">W</div>
@@ -40,3 +64,4 @@ function insertTable(data) {
 }
 
 const data = loadTable();
+loadTable()
