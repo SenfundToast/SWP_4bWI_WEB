4f0042f66afbe8a36e2d2329fcfba4c5f8cbef2c
mathias <mathias@beirer.com>
2023-12-20 11:10:48 +0100

    football_react

--- Football_Bl/script2.js
+++ Football_Bl/script2.js
@@ -6,3 +6,25 @@ fetch("https://api.openligadb.de/getbltable/bl1/2023").then((res) => res.json().
     let team = data.find((team) => (team.teamInfoId == id));
     console.log(team);
 }))
+
+
+{
+    /* <div class="bg-Bg2 h-[10vh] max-w-screen grid grid-cols-3 items-center text-center text-white ">
+                <div class="grid grid-cols-5">
+                    <div class="col-span-1"></div>
+                    <div class="col-span-2 ">LOGO</div>
+                    <div class="col-span-2"></div>
+                </div>
+
+                <div class="grid grid-cols-6">
+                    <div class="col-span-6 text-3xl">FC Bayern Münschen</div>
+
+                </div>
+
+                <div class="grid grid-cols-5">
+                    <div class="col-span-2"></div>
+                    <div class="col-span-2">LOGO</div>
+                    <div class="col-span-1"></div>
+                </div>
+            </div> */
+}
