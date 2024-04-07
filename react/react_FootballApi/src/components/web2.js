67db05c7b39595469cf2c8ce4a75b5b4ee889247
mathias <mathias@beirer.com>
2024-04-07 14:13:05 +0200

    Test

--- react/react_FootballApi/src/components/web2.js
+++ react/react_FootballApi/src/components/web2.js
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-const urlParams = new URLSearchParams(window.location.search);
-const id = urlParams.get('id');
+export default function Web2() {
+    const [team, setTeam] = useState(null);
 
+    useEffect(() => {
+        fetchTeam();
+    }, []);
 
+    const fetchTeam = () => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const id = urlParams.get('id');
 
-export default function web2() {
-  return (
-    <div>web2</div>
-  )
+        fetch("https://api.openligadb.de/getbltable/bl1/2023")
+            .then((res) => res.json())
+            .then((data) => {
+                const foundTeam = data.find((team) => team.teamInfoId === id);
+                setTeam(foundTeam);
+            })
+            .catch((error) => {
+                console.error('Error fetching team data:', error);
+            });
+    };
+
+    return (
+        <div className="bg-Bg2 h-screen">
+            <div className="bg-Bg2 h-[10vh] max-w-screen grid grid-cols-3 items-center text-center text-white ">
+                <div className="grid grid-cols-5">
+                    <div className="col-span-1"></div>
+                    <div className="col-span-2">LOGO</div>
+                    <div className="col-span-2"></div>
+                </div>
+                <div className="grid grid-cols-6">
+                    <div className="col-span-6 text-3xl">{team ? team.teamName : ''}</div>
+                </div>
+                <div className="grid grid-cols-5">
+                    <div className="col-span-2"></div>
+                    <div className="col-span-2">LOGO</div>
+                    <div className="col-span-1"></div>
+                </div>
+            </div>
+            <a className="bg-yellow w-2/4 h-28 left-1/4 bottom-10  border-yellow rounded-xl absolute flex justify-center items-center text-black text-4xl font-hind" href="web.html">View Full Table</a>
+        </div>
+    );
 }
-
-fetch("https://api.openligadb.de/getbltable/bl1/2023").then((res) => res.json().then((data) => {
-    let team = data.find((team) => (team.teamInfoId === id));
-    console.log(team);
-}))
-
